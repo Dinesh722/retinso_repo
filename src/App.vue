1b2c7cacaec159e4725705f5f5a54055a86cad80
Dinesh <dnsh.dinesh97@gmail.com>
2020-04-25 04:12:05 -0700

    Retinso

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div>
+    <nav class="navbar navbar-expand-lg scrolling-navbar navbar-light bg-light fixed-top">
+      <div class="navbar-brand row">
+        <div class="col-md-12 col-12">
+          <span >
+            <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/ee037401cb5d31b23cf780808ee4ec1f.svg">
+          </span>
+          <!-- <span class="col-md-1 col-1">
+            Sign In
+          </span> -->
+        </div>
+      </div>
+    </nav>
+    </div>
+
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+       <div class="bg-dark text-light">
+      <br><br>
+      <div class="container">
+        <div class="col-md-12 row">
+          <div class="col-md-6">
+            <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/c266ad32e5e88af804b3a1b6b60098f9.svg">
+          </div>
+          
+          <div class="col-md-3"><br>
+            <div :key="level" v-for="level in col2">
+              <div>
+                {{level}}
+              </div><br>
+            </div>
+          </div>
+
+          <div class="col-md-3"><br>
+           <div :key="level" v-for="level in col3">
+              <div>
+                {{level}}
+              </div><br>
+            </div>
+           </div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      col2:[],
+      col3:[]
+    }
+  },
+  created(){
+    this.col2 = [
+      'Read our blog',
+      'Create a business account',
+      'About Uber Eats',
+      'Buy gift cards',
+      'Add your restaurant',
+      'Sign up to deliver'
+    ];
+    this.col3 = [
+      'Get Help',
+      'Read FAQs',
+      'View all cities',
+      'View all countries',
+      'Restaurants near me',
+      'English'
+    ]
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,219 @@
+<template>
+  <div>
+    <div class="bg">
+      <div class="text-center col-md-12" style="margin-top:15%">
+        <strong><h2>Your favorite food, delivered with Uber</h2></strong>
+      </div>
+
+      <div class="text-center col-md-12 col-12">
+          <input type="text" style="width:50%" placeholder="Enter delivery address"> <button type="button" class="btn btn-dark">Find Food</button>
+      </div>
+    </div>
+
+    <div class="container" style="margin-top:2%">
+      <div class="font-weight-bold">
+        <h4>Food Delivery in San Francisco Bay Area</h4>
+      </div>
+      <br>
+      <div class="row col-md-12">
+        <div class="col-md-4">
+          <img src="https://duyt4h9nfnj50.cloudfront.net/sku/91832b4d0535174c795540ffebe4b03b">
+          <div><span class="font-weight-bold">McDonald's® (Fillmore)</span><br>Delivery Fee is $2.99</div>
+          <hr>
+          <h6 class="font-weight-normal">American • Fast Food • Burgers</h6>
+        </div>
+        <div class="col-md-4">
+          <img src="https://d1ralsognjng37.cloudfront.net/704043aa-6cc2-4515-bbfd-80dfc66b9808.jpeg">
+          <div><span class="font-weight-bold">Starbucks® (Polk Street)</span><br>Delivery Fee is $3.99</div>
+          <hr>
+          <h6 class="font-weight-normal">Cafe • Coffee & Tea • Breakfast and Brunch • Bakery</h6>
+        </div>
+        <div class="col-md-4">
+          <img src="https://d1ralsognjng37.cloudfront.net/4f34125f-afe1-4cbe-9e91-e1fd46befcc8.jpeg">
+          <div><span class="font-weight-bold">Burger King (819 Van Ness Avenue)</span><br>Delivery Fee is $1.99</div>
+          <hr>
+          <h6 class="font-weight-normal">Breakfast and Brunch • Sandwich</h6>
+        </div>
+      </div>
+      <hr>
+
+      <div class="row col-md-12">
+          <div class="text-right col-md-6 col-6">
+            <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/36c9cff040dfeea80ec0853b81261349.svg">
+          </div>
+
+          <div class="col-md-6 col-6 row text-left">
+            <div class="col-md-12">
+              <h5>There's more to love in the app.</h5>
+            </div>
+            <div class="col-md-12">
+            </div>
+          </div>
+      </div>
+      <hr>
+
+      <div class="font-weight-bold">
+        <h4>New on Uber Eats</h4>
+      </div>
+      <br>
+      <div class="row col-md-12">
+        <div class="col-md-4">
+          <img src="https://d1ralsognjng37.cloudfront.net/fd1e3d28-37a1-4d42-85bb-4b0bbf4f7acd.jpeg">
+          <div><span class="font-weight-bold">Philz Coffee (4298 24th Street)</span><br>Delivery Fee is $3.99</div>
+          <hr>
+          <h6 class="font-weight-normal">Cafe • Coffee and Tea • Breakfast and Brunch</h6>
+        </div>
+      </div>
+      <hr>
+      
+      <div class="row col-md-12">
+        <div class="col-md-4">
+          <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/cef389b486cb4827e6ba007f26ebddab.svg">
+          <div><h4>Feed your employees</h4></div>
+          <div><h6 class="text-success">Create a business account</h6></div>
+        </div>
+
+        <div class="col-md-4">
+          <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/7f56b34e6c253cb54a35bacf5150dde9.svg">
+          <div><h4>Your restaurant, delivered</h4></div>
+          <div><h6 class="text-success">Add your restaurant</h6></div>
+        </div>
+
+        <div class="col-md-4">
+          <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/84d6770ca439c4b1ba2d6f53adc1d039.svg">
+          <div><h4>Deliver the Eats</h4></div>
+          <div><h6 class="text-success">Sign up to deliver</h6></div>
+        </div>
+      </div>
+      <hr>
+
+       <div class="font-weight-bold">
+        <h4>Pizza Delivery in San Francisco Bay Area</h4>
+      </div>
+      <br>
+      <div class="row col-md-12">
+        <div class="col-md-4">
+          <img src="https://duyt4h9nfnj50.cloudfront.net/resized/1532646450835-w550-bf.jpg">
+          <div><span class="font-weight-bold">All Nite Pizza</span><br>Delivery fee is $7.99.</div>
+          <hr>
+          <small class="font-weight-normal">Pizza</small>
+        </div>
+        <div class="col-md-4">
+          <img src="https://duyt4h9nfnj50.cloudfront.net/resized/1521603815569-w550-0c.jpg">
+          <div><span class="font-weight-bold">Cool Cravings - Divisadero</span><br>Delivery fee is $4.99.</div>
+          <hr>
+          <small class="font-weight-normal">American • Breakfast and Brunch • Juice and Smoothies • Salads • Sandwich</small>
+        </div>
+        <div class="col-md-4">
+          <img src="https://duyt4h9nfnj50.cloudfront.net/resized/1531945584353-w550-c2.jpg">
+          <div><span class="font-weight-bold">Mr Pizza Man</span><br>Delivery Fee is $1.99</div>
+          <hr>
+          <small class="font-weight-normal">Pizza</small>
+        </div>
+      </div>
+      <hr>
+
+      <div>
+        <div class="row col-md-12 col-12">
+        <h3 class="col-md-6 col-10">Food Near Me</h3>
+        </div>
+      </div>
+        <br>
+        <div class="row col-md-12">
+        <div class="col-md-2">
+          <img class="rnd" src="https://www.tasteofhome.com/wp-content/uploads/2017/10/Chicken-Pizza_exps30800_FM143298B03_11_8bC_RMS-2-696x696.jpg">
+          <div  class="font-weight-bold text-center">Pizza</div>
+        </div>
+
+        <div class="col-md-2">
+          <img class="rnd" src="https://media1.popsugar-assets.com/files/thumbor/KunGv8zUe_VC_Mj7xJB7NrCU7m0/147x0:1325x1178/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/08/21/050/n/44554866/78977e545d5dde1acc8c03.59502138_/i/best-fast-food-chicken-nuggets.png">
+          <div  class="font-weight-bold text-center">Fast Food</div>
+        </div>
+
+        <div class="col-md-2">
+          <img class="rnd" src="https://images.britcdn.com/wp-content/uploads/2014/07/strawberry-pretzel-salad-sugar-free-sq.jpg">
+          <div  class="font-weight-bold text-center">Desserts</div>
+        </div>
+
+        <div class="col-md-2">
+          <img class="rnd" src="https://cdn.i-scmp.com/sites/default/files/styles/768x768/public/d8/images/methode/2020/04/22/9ac2ff2a-83e0-11ea-8863-2139a14b0dea_image_hires_003008.JPG?itok=MB2_g87G&v=1587486621">
+          <div  class="font-weight-bold text-center">Bubble Tea</div>
+        </div>
+
+        <div class="col-md-2">
+          <img class="rnd" src="https://www.thespruceeats.com/thmb/8s5f39NMWBrpbvGLPr2dzMpFWG8=/2733x2733/smart/filters:no_upscale()/chinese-take-out-473784604-57d31f7f3df78c5833464853.jpg">
+          <div  class="font-weight-bold text-center">Chinese</div>
+        </div>
+
+        <div class="col-md-2">
+          <img class="rnd" src="https://img.huffingtonpost.com/asset/5b9e115c250000360036b32a.jpeg?ops=scalefit_960_noupscale">
+          <div  class="font-weight-bold text-center">American</div>
+        </div>
+        </div>
+        <hr>
+
+        <div>
+        <h3>Countries with Uber Eats</h3>
+        </div>  
+        <br>
+        <div class="col-md-12 col-12 row">
+          <div class="col-md-3 col-6" :key="level" v-for="level in countries">
+            <h6 class="font-weight-normal">{{level}}</h6><br>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .bg{
+  background-image: url("https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/2d72424dcc82f116a7d8850d1f44ec09.svg");
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  }
+  img {
+    max-width: 100%;
+    max-height: 100%;
+}
+.rnd{
+   border-radius: 50%;
+}
+</style>
+
+<script>
+export default {
+  data(){
+    return {
+      countries:[]
+    }
+  },
+  created(){
+    this.countries = ['United States',
+'Brazil',
+'Spain',
+'Mexico',
+'France',
+'Canada',
+'United Kingdom',
+'Australia',
+'Japan',
+'Poland',
+'Italy',
+'Portugal',
+'Chile',
+'Colombia',
+'Taiwan (ROC)',
+'Belgium',
+'South Africa',
+'Switzerland',
+'Ukraine',
+'New Zealand',
+'Peru',
+'Saudi Arabia',
+'Ireland',
+'Romania']
+  }
+}
+</script>
